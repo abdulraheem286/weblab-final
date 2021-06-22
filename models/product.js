4f678df5fc8b2fa498ff01a66d47e78bf526e9f9
Abdul Raheem <61801382+abdulraheem286@users.noreply.github.com>
2021-06-22 17:38:24 +0500

    v16

--- models/product.js
+++ models/product.js
@@ -3,7 +3,7 @@ var mongoose = require("mongoose");
 var productSchema = mongoose.Schema({
   name: String,
   price: String,
-  description: String,
+  discription: String,
   rating: Number,
 });
 const Product = mongoose.model("Product", productSchema);
