3de3ea72d1e4325db3ca02466a3887b9e3f5ef5e
Abdul Raheem <61801382+abdulraheem286@users.noreply.github.com>
2021-06-22 12:48:44 +0500

    v9

--- routes/products.js
+++ routes/products.js
@@ -6,7 +6,7 @@ var checkSessionAuth = require("../middlewares/checkSessionAuth");
 router.get("/", async function (req, res, next) {
   let products = await Product.find();
   console.log(req.session.user);
-  res.render("products/list", { title: "Products In DB", products });
+  res.render("products/list", { title: "Web Final Lab", products });
 });
 
 router.get("/cart", function (req, res, next) {
